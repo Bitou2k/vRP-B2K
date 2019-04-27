cafa5546c367ce379a9c483787392453625a1568
Stan <news.stan@gmail.com>
2019-04-27 03:07:55 -0300

    Updated Chat for accepting / commands
    Updated Chat for accepting / commands, for exemple: /dv
    Chat atualizado para aceitar comandos com /, por exemplo: /dv

--- resources/[essentials]/chat/sv_chat.lua
+++ resources/[essentials]/chat/sv_chat.lua
@@ -49,7 +49,7 @@ end)
 AddEventHandler('__cfx_internal:commandFallback', function(command)
     local name = GetPlayerName(source)
 
-    --TriggerEvent('chatMessage', source, name, '/' .. command)
+    TriggerEvent('chatMessage', source, name, '/' .. command)
 
     if not WasEventCanceled() then
         --TriggerClientEvent('chatMessage', -1, name, { 255, 255, 255 }, '/' .. command) 
